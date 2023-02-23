5df6d8c0a6894dcc85f442b9554bf816ab7bd679
Dzianis Perahudau <tvigger@yandex.ru>
2023-02-23 21:42:24 +0300

    feat: add guessing-game code js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    
+    constructor() { 
+       
+     }
 
-    setRange(min, max) {
-
+    setRange(min, max) {     
+        this.min = min;
+        this.max = max;
+       /* this.midd = 0;*/
     }
 
     guess() {
-
+        while (this.min < this.max){
+            return this.midd = Math.round(this.min + Math.abs(((this.max - this.min) / 2)));
+            
+    };
+    if(this.min === this.max){
+        return this.midd;
     }
-
+}
     lower() {
-
+        this.max = this.midd;
     }
-
     greater() {
-
+        this.min = this.midd;
     }
 }
 
